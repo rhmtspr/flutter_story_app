9ff3fbe8911e830e5af51978e765362ad0d5a2e9
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-02 02:05:52 +0800

    add user model and adjust it for targeting api

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -1,6 +1,6 @@
-import 'package:declarative_navigation/db/auth_repository.dart';
-import 'package:declarative_navigation/model/user.dart';
 import 'package:flutter/material.dart';
+import '../db/auth_repository.dart';
+import '../model/user.dart';
 
 class AuthProvider extends ChangeNotifier {
   final AuthRepository authRepository;
@@ -12,49 +12,49 @@ class AuthProvider extends ChangeNotifier {
   bool isLoadingRegister = false;
   bool isLoggedIn = false;
 
+  /// LOGIN
   Future<bool> login(User user) async {
     isLoadingLogin = true;
     notifyListeners();
 
-    final userState = await authRepository.getUser();
-    if (user == userState) {
-      await authRepository.login();
+    try {
+      await authRepository.login(user);
+      isLoggedIn = true;
+    } catch (e) {
+      isLoggedIn = false;
     }
-
-    isLoggedIn = await authRepository.isLoggedIn();
 
     isLoadingLogin = false;
     notifyListeners();
-
     return isLoggedIn;
   }
 
-  Future<bool> logout() async {
-    isLoadingLogout = true;
-    notifyListeners();
-
-    final logout = await authRepository.logout();
-    if (logout) {
-      await authRepository.deleteUser();
-    }
-
-    isLoggedIn = await authRepository.isLoggedIn();
-
-    isLoadingLogout = false;
-    notifyListeners();
-
-    return !isLoggedIn;
-  }
-
-  Future<bool> saveUser(User user) async {
+  /// REGISTER
+  Future<bool> register(User user, String name) async {
     isLoadingRegister = true;
     notifyListeners();
 
-    final userState = await authRepository.saveUser(user);
+    try {
+      await authRepository.register(user, name: name);
+      isLoadingRegister = false;
+      notifyListeners();
+      return true;
+    } catch (_) {
+      isLoadingRegister = false;
+      notifyListeners();
+      return false;
+    }
+  }
 
-    isLoadingRegister = false;
+  /// LOGOUT
+  Future<void> logout() async {
+    isLoadingLogout = true;
     notifyListeners();
 
-    return userState;
+    await authRepository.logout();
+    isLoggedIn = false;
+
+    isLoadingLogout = false;
+    notifyListeners();
   }
 }
