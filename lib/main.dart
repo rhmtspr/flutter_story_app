d1103ce195593208bac92a838317a41f0fdc1c27
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-02 21:01:43 +0800

    update change notifier provider to multi provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:declarative_navigation/db/auth_repository.dart';
+import 'package:declarative_navigation/db/story_repository.dart';
 import 'package:declarative_navigation/provider/auth_provider.dart';
+import 'package:declarative_navigation/provider/story_provider.dart';
 import 'package:declarative_navigation/routers/router_delegate.dart';
 import 'package:declarative_navigation/services/api_service.dart';
 import 'package:flutter/material.dart';
@@ -31,8 +33,23 @@ class _QuotesAppState extends State<QuotesApp> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => authProvider,
+    return MultiProvider(
+      providers: [
+        Provider(create: (_) => ApiService()),
+
+        ProxyProvider<ApiService, AuthRepository>(
+          update: (_, api, __) => AuthRepository(api),
+        ),
+
+        ChangeNotifierProxyProvider<AuthRepository, AuthProvider>(
+          create: (_) => AuthProvider(AuthRepository(ApiService())),
+          update: (_, repo, __) => AuthProvider(repo),
+        ),
+
+        ChangeNotifierProvider(
+          create: (context) => StoryProvider(ApiService()),
+        ),
+      ],
       child: MaterialApp(
         title: 'Quotes App',
         home: Router(
