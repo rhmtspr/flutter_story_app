8dc9edc2bdd67bca6442233212575991a1361608
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-02 21:04:23 +0800

    create upload story that returns upload response

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:declarative_navigation/model/upload_response.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
@@ -45,5 +46,48 @@ class ApiService {
     }
 
     return data['loginResult']; // contains id, name, token
+  }
+
+  Future<UploadResponse> uploadStory({
+    required List<int> bytes,
+    required String fileName,
+    required String description,
+    required String token,
+    double? lat,
+    double? lon,
+  }) async {
+    final uri = Uri.parse('$baseUrl/stories');
+
+    final request = http.MultipartRequest('POST', uri);
+
+    /// Add Authorization Header ✅
+    request.headers['Authorization'] = 'Bearer $token';
+
+    /// Add fields
+    request.fields['description'] = description;
+
+    if (lat != null) {
+      request.fields['lat'] = lat.toString();
+    }
+
+    if (lon != null) {
+      request.fields['lon'] = lon.toString();
+    }
+
+    /// Add file
+    request.files.add(
+      http.MultipartFile.fromBytes('photo', bytes, filename: fileName),
+    );
+
+    final streamedResponse = await request.send();
+    final response = await http.Response.fromStream(streamedResponse);
+
+    final uploadResponse = UploadResponse.fromJson(response.body);
+
+    if (response.statusCode != 201 || uploadResponse.error) {
+      throw Exception(uploadResponse.message);
+    }
+
+    return uploadResponse;
   }
 }
