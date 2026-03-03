3223221a55e09dc8fa7f7a2803508e19bb2a219c
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-04 02:56:38 +0800

    create story detail provider

---
+++ lib/provider/story_detail_provider.dart
@@ -0,0 +1,39 @@
+import 'package:declarative_navigation/provider/story_detail_result_state.dart';
+import 'package:declarative_navigation/services/api_service.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StoryDetailProvider extends ChangeNotifier {
+  final String tokenKey = 'token';
+  final ApiService _apiService;
+  StoryDetailProvider(this._apiService);
+  StoryDetailResultState _resultState = StoryDetailNoneState();
+  StoryDetailResultState get resultState => _resultState;
+
+  Future<void> fetchStoryDetail(String id) async {
+    try {
+      _resultState = StoryDetailLoadingState();
+      notifyListeners();
+
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString(tokenKey);
+
+      if (token == null) {
+        throw Exception('User not logged in');
+      }
+
+      final result = await _apiService.getStoryDetail(id, token);
+
+      if (result.error) {
+        _resultState = StoryDetailErrorState(result.message);
+        notifyListeners();
+      } else {
+        _resultState = StoryDetailLoadedState(result.story);
+        notifyListeners();
+      }
+    } on Exception catch (e) {
+      _resultState = StoryDetailErrorState(e.toString());
+      notifyListeners();
+    }
+  }
+}
