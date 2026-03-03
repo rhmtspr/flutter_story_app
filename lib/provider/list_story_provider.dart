d9b6cd2368ae3eb4e397ef3c6d626822b96fa020
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-04 02:56:13 +0800

    add token and if for null checking

--- lib/provider/list_story_provider.dart
+++ lib/provider/list_story_provider.dart
@@ -1,8 +1,10 @@
 import 'package:declarative_navigation/provider/list_story_result_state.dart';
 import 'package:declarative_navigation/services/api_service.dart';
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ListStoryProvider extends ChangeNotifier {
+  final String tokenKey = 'token';
   final ApiService _apiService;
   ListStoryProvider(this._apiService);
   ListStoryResultState _resultState = ListStoryNoneState();
@@ -13,7 +15,14 @@ class ListStoryProvider extends ChangeNotifier {
       _resultState = ListStoryLoadingState();
       notifyListeners();
 
-      final result = await _apiService.getListStory();
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString(tokenKey);
+
+      if (token == null) {
+        throw Exception('User not logged in');
+      }
+
+      final result = await _apiService.getListStory(token);
 
       if (result.error) {
         _resultState = ListStoryErrorState(result.message);
