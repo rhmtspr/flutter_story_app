ff01c0579877bf5686bc0edbd30ce2f19a025849
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-03 22:06:19 +0800

    create story detail screen

---
+++ lib/screen/detail/story_detail_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StoryDetail extends StatefulWidget {
+  const StoryDetail({super.key});
+
+  @override
+  State<StoryDetail> createState() => _StoryDetailState();
+}
+
+class _StoryDetailState extends State<StoryDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
