fa689867fc61987885d09f22413e31387dc9e4ae
rachmatrisky <rachmatrisky5@gmail.com>
2026-03-04 02:53:15 +0800

    remove description and createdAt

--- lib/widgets/story_card.dart
+++ lib/widgets/story_card.dart
@@ -1,26 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class StoryCard extends StatelessWidget {
   final String name;
-  final String description;
   final String photoUrl;
-  final DateTime createdAt;
   final VoidCallback onTap;
 
   const StoryCard({
     super.key,
     required this.name,
-    required this.description,
     required this.photoUrl,
-    required this.createdAt,
     required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    final formattedDate = DateFormat("dd MMM yyyy • HH:mm").format(createdAt);
-
     return InkWell(
       borderRadius: BorderRadius.circular(16),
       onTap: onTap,
@@ -58,37 +51,6 @@ class StoryCard extends StatelessWidget {
                       fontSize: 18,
                       fontWeight: FontWeight.w600,
                     ),
-                  ),
-
-                  const SizedBox(height: 6),
-
-                  // DESCRIPTION
-                  Text(
-                    description,
-                    style: TextStyle(fontSize: 14, color: Colors.grey.shade700),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-
-                  const SizedBox(height: 12),
-
-                  // DATE
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.schedule,
-                        size: 14,
-                        color: Colors.grey.shade500,
-                      ),
-                      const SizedBox(width: 6),
-                      Text(
-                        formattedDate,
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.grey.shade500,
-                        ),
-                      ),
-                    ],
                   ),
                 ],
               ),
